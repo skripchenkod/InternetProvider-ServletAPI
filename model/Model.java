a88363d3de0ee7b3e2953ee7ce5e45b6642b2fbf
Skripchenkod <skrypchenko.dm@icloud.com>
2020-08-10 18:46:01 +0300

    Commit 1

---
+++ model/Model.java
@@ -0,0 +1,23 @@
+package ua.skrypchenko.taskmvc.model;
+
+public class Model {
+
+    private int First;
+    private int Second;
+
+    public int getFirst() {
+        return First;
+    }
+
+    public void setFirst(int first) {
+        First = first;
+    }
+
+    public int getSecond() {
+        return Second;
+    }
+
+    public void setSecond(int second) {
+        Second = second;
+    }
+}
