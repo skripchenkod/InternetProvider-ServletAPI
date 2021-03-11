216938cc50ee3bb55765379528285cddc9096ac5
Skripchenkod <skrypchenko.dm@icloud.com>
2021-03-11 22:29:43 +0200

    add test

--- src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
+++ src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
@@ -1,5 +1,21 @@
 package com.epam.rd.java.basic.practice1;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class MainTest {
 
+    @Test
+    public void shouldTestMain() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        Main.printHelloWorld(printStream);
+        String actualString = byteArrayOutputStream.toString();
+
+        Assert.assertEquals("Hello, World", actualString );
+    }
 }
