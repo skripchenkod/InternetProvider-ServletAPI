e5e7e823a0b2db5835f69b7978ebddb3ab96f0c2
Skripchenkod <skrypchenko.dm@icloud.com>
2021-03-11 16:00:30 +0200

    new version

--- src/main/java/com/epam/rd/java/basic/practice1/Main.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Main.java
@@ -4,7 +4,7 @@ package com.epam.rd.java.basic.practice1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World")
+        System.out.print("Hello, World");
         
     }
 
