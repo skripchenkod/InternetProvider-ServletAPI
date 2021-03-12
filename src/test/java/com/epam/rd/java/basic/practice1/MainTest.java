842e904ae598b7ecee35e2faaded34f9eda71b2b
Skripchenkod <skrypchenko.dm@icloud.com>
2021-03-12 13:18:47 +0200

    refactor test

--- src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
+++ src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
@@ -2,10 +2,14 @@ package com.epam.rd.java.basic.practice1;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MainTest {
 
     @Test
     public void shouldTestMain() {
-        Main.main(null);
+        String[] args = {"1", "2"};
+        Main.main(args);
+        assertEquals(args[0], "1");
     }
 }
