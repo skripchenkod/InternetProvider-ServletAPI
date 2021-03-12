b08b165d28dcc485e8f67371f2e939c2842be737
Skripchenkod <skrypchenko.dm@icloud.com>
2021-03-12 13:02:50 +0200

    refactor tests

--- src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
+++ src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
@@ -1,21 +1,11 @@
 package com.epam.rd.java.basic.practice1;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 public class MainTest {
 
     @Test
     public void shouldTestMain() {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-
-        Main.printHelloWorld(printStream);
-        String actualString = byteArrayOutputStream.toString();
-
-        Assert.assertEquals("Hello, World", actualString );
+        Main.main(null);
     }
 }
